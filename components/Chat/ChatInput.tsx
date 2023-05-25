9e24ac6354964c148e61f5a91a7c37b6ca15dc0f
Tristan Yang <yanggc888@163.com>
2023-05-25 17:36:46 +0800

    refactor: updates

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -274,7 +274,7 @@ export const ChatInput = ({
         <div className="relative mx-2 flex w-full flex-grow flex-col rounded-md border border-black/10 bg-white shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:border-gray-900/50 dark:bg-[#40414F] dark:text-white dark:shadow-[0_0_15px_rgba(0,0,0,0.10)] sm:mx-4">
           <textarea
             ref={textareaRef}
-            className="m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 pl-10 text-black dark:bg-transparent dark:text-white md:py-3 md:pl-10"
+            className="outline-none m-0 w-full resize-none border-0 bg-transparent p-0 py-2 pr-8 text-black dark:bg-transparent dark:text-white md:py-3 pl-3"
             style={{
               resize: 'none',
               bottom: `${textareaRef?.current?.scrollHeight}px`,
--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useQuery } from 'react-query';
+import { useQuery } from '@tanstack/react-query';
 
 import { GetServerSideProps } from 'next';
 import { useTranslation } from 'next-i18next';
